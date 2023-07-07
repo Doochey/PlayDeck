1a1eb1ee8decb409df91df1ceceb139912fe97d2
Doochey <doochey@live.co.uk>
2023-07-07 16:15:59 +0100

    Reactive Edit
    Edit button on card now displays modal, saving edits replaces the card with a new one with no page refresh.

--- PlayDeckRazor/Pages/Games/Gateway.cshtml.cs
+++ PlayDeckRazor/Pages/Games/Gateway.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.EntityFrameworkCore;
 using NuGet.Protocol;
 using PlayDeckRazor.Data;
@@ -38,4 +39,52 @@ public class GatewayModel : PageModel
         Game = game;
         return new OkObjectResult(game.ToJson());
     }
+
+    public async Task<IActionResult> OnPostEditAsync()
+    {
+        if (!ModelState.IsValid)
+        {
+            return new BadRequestResult();
+        }
+
+        if (GameExists(Game.ID))
+        {
+            _context.Attach(Game).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GameExists(Game.ID))
+                {
+                    return new NotFoundResult();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            ViewData["GameCard"] = Game;
+            var model = new IndexModel(_context);
+            var test = new ViewDataDictionary(new Microsoft.AspNetCore.Mvc.ModelBinding.EmptyModelMetadataProvider(),
+                new Microsoft.AspNetCore.Mvc.ModelBinding.ModelStateDictionary()) { { "IndexModel", model } };
+            test.Model = model;
+            test["GameCard"] = Game;
+            return new PartialViewResult()
+            {
+                ViewName = "_GameCard",
+                ViewData = test,
+            };
+        }
+        return new BadRequestResult();
+    }
+    
+    private bool GameExists(int id)
+    {
+        return (_context.Game?.Any(e => e.ID == id)).GetValueOrDefault();
+    }
+    
+        
 }
--- PlayDeckRazor/wwwroot/js/site.js
+++ PlayDeckRazor/wwwroot/js/site.js
@@ -1,7 +1,7 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 document.addEventListener('alpine:init', () => {
-    Alpine.data('gameData', () => ({
+    Alpine.store('gameData', {
         gameTitle: '',
         gameId: '',
         deckId: '',
@@ -11,9 +11,37 @@ document.addEventListener('alpine:init', () => {
         completeDate: '',
         rating: '',
         playTime: '',
-        lastPlayed: ''
-    }))
+        lastPlayed: '',
+        mode: '',
+
+        setInfo(id, title) {this.gameId = id; this.gameTitle=title;},
+        setInfoFull(id, title, deckId, imageUrl, playStatus, startDate, completeDate, rating, playTime, lastPlayed) {
+            this.gameId = id ?? null;
+            this.gameTitle = title ?? null;
+            this.deckId = deckId ?? null;
+            this.imageUrl = imageUrl ?? null;
+            this.playStatus = playStatus ?? null;
+            this.startDate = startDate ?? null;
+            this.completeDate = completeDate ?? null;
+            this.rating = rating ?? null;
+            this.playTime = playTime ?? null;
+            this.lastPlayed = lastPlayed ?? null;
+        }
+    })
 })
+
+async function GetGameDetails(id) {
+    await fetch('https://localhost:6610/Games/Gateway/retrieve/'+ id, {
+        method: 'GET'})
+        .then(response => response.text())
+        .then(data => {
+            let jsonResponse = JSON.parse(data);
+            Alpine.store('gameData').setInfoFull(jsonResponse['ID'], jsonResponse['Title'], jsonResponse['DeckID'], jsonResponse['ImageURL'],
+                jsonResponse['PlayStatus'], jsonResponse['StartDate']?.split("T")[0], jsonResponse['CompleteDate']?.split("T")[0], jsonResponse['Rating'],
+                jsonResponse['PlayTime'], jsonResponse['LastPlayed']?.split("T")[0]);
+        })
+        .catch(errorMsg => { console.log(errorMsg); });
+}
 
 
 
@@ -25,8 +53,8 @@ function ShowToast(operation, text) {
     toast.show()
 }
 
-function SendDelete() {
-    fetch('https://localhost:6610/index?handler=delete', {
+async function SendDelete() {
+    await fetch('https://localhost:6610/index?handler=delete', {
         method: 'POST',
         body: new FormData(document.querySelector('#delete-modal-form'))})
         .then(response => response.text())
@@ -37,24 +65,32 @@ function SendDelete() {
         .catch(errorMsg => { console.log(errorMsg); });
 }
 
-async function GetGameDetails(id, {gameTitle, gameId, deckId, imageUrl, playStatus, startDate, completeDate, rating, playTime, lastPlayed}) {
-    await fetch('https://localhost:6610/Games/Gateway/retrieve/'+ id, {
-        method: 'GET'})
+async function SendEdit(id) {
+    await fetch('https://localhost:6610/Games/Gateway/edit/', {
+        method: 'POST',
+        body: new FormData(document.querySelector('#edit-modal-form'))})
         .then(response => response.text())
         .then(data => {
-            let jsonResponse = JSON.parse(data);
-            gameTitle = jsonResponse['Title'];
-            gameId: jsonResponse['gameId'];
-            deckId: jsonResponse['deckId'];
-            imageUrl: jsonResponse['imageUrl'];
-            playStatus: jsonResponse['playStatus'];
-            startDate: jsonResponse['startDate'];
-            completeDate: jsonResponse['completeDate'];
-            rating: jsonResponse['rating'];
-            playTime: jsonResponse['playTime'];
-            lastPlayed: jsonResponse['lastPlayed'];
-            alert(gameTitle);
+            newItem = document.createElement('div');
+            newItem.innerHTML = data;
+            oldItem =  document.querySelector('#game-card-' + id);
+            oldItem.parentNode.replaceChild(newItem, oldItem);
+            ShowToast('Edit', 'was modified');
         })
         .catch(errorMsg => { console.log(errorMsg); });
 }
+
+async function SendAdd() {
+    await fetch('https://localhost:6610/index?handler=delete', {
+        method: 'POST',
+        body: new FormData(document.querySelector('#delete-modal-form'))})
+        .then(response => response.text())
+        .then(data => {
+            document.querySelector('#game-card-' + data).remove();
+            ShowToast('Deletion', 'was removed from the database');
+        })
+        .catch(errorMsg => { console.log(errorMsg); });
+}
+
+
 
